6a145ea4fee31d4cb7dcaf27b39bfcddb692515e
Letícia Faria <leticianfaria@hotmail.com>
2018-08-30 16:43:19 -0300

    Refatorando código

--- script/script.js
+++ script/script.js
@@ -1,31 +1,27 @@
 $(document).ready(function () {
-  $(".btn").on("click", addTweet).on("click", buttonClick);
-  $(".tweet-input").bind("keyup keydown keypress", insertChar);
-  $(".edit").on("click",edit);
-  $(".save").on("click",save);
-
-  setInterval(changeColorCount, 1);
-  setInterval(disable, 1);
-  setInterval(resizeTextfield(tweetInput), 1);
+  setFunctions();
 });
 
-// VARIÁVEIS GLOBAIS
-let tweets = document.querySelector(".tweets");
-let button = document.querySelector(".btn");
-let tweetInput = document.querySelector(".tweet-input");
-let numberChar = document.querySelector(".numberChar");
-const maxChar = 140;
-let count = 0;
-let valueLength;
-// MUDANDO INFORMAÇÕES DO PROFILE
-let name = document.querySelector(".name");
-let user = document.querySelector(".username");
-let btnEdit = document.querySelector(".edit");
-let btnSave = document.querySelector(".save");
+let setFunctions = () => {
+  const btnEdit = $(".edit");
+  const btnSave = $(".save");
+  const numberChar = $(".numberChar");
+  const valueLength;
+  const maxChar = 140;
 
+  $(".btn").on("click", addTweet).click( () => buttonClick(numberChar));
+  $(".tweet-input").bind("keyup keydown keypress", () => insertChar(numberChar, valueLength, maxChar));
+  btnEdit.click( () => edit($(".name"), $(".username"), btnSave, btnEdit));
+  btnSave.click( () => save($(".name"), $(".username"), btnSave, btnEdit));
+
+  setInterval( () => changeColorCount(numberChar, valueLength, maxChar), 1);
+  setInterval(disable, 1);
+  setInterval(resizeTextfield($(".tweet-input")), 1);
+
+}
 
 // FUNÇÃO AO CLICAR NO BOTÃO TWEET
-function addTweet() {
+var addTweet = () => {
   let elements = `
   <div class="box">
     <div class="img-profile"></div>
@@ -37,43 +33,44 @@ function addTweet() {
 }
 
 // EVENTO DE BOTÃO
-function buttonClick() {
+var buttonClick = () => {
+  let count = 0;
   $(".count-tweets").text(count += 1);
   $(".tweet-input").val("").height("85px");
   $(".numberChar").text(maxChar);
 }
 
 // CONTADOR DE CARACTERES
-function insertChar() {
+let insertChar = (number, valueLength, maxChar) => {
   valueLength = $(".tweet-input").val().length;
   if (valueLength) {
-    $(".numberChar").text(maxChar - valueLength);
+    number.text(maxChar - valueLength);
   } else if ($(".tweet-input").val() === "") {
-    $(".numberChar").text(maxChar);
+    number.text(maxChar);
   }
 }
 
 // CORES DO CONTADOR
-function changeColorCount() {
-  valueLength = document.querySelector(".tweet-input").value.length;
+let changeColorCount = (number, valueLength, maxChar) => {
+  valueLength = $(".tweet-input").val().length;
   if (valueLength >= 120 && valueLength < 130) {
-    numberChar.style.color = "yellow";
+    number.css("color","yellow");
   } else if (valueLength >= 130 && valueLength < 140) {
-    numberChar.style.color = "red";
+    number.css("color","red");
   } else if (valueLength > 140) {
-    numberChar.style.color = "#c1c1c1";
+    number.css("color","#c1c1c1");
   } else if (valueLength < 120) {
-    numberChar.style.color = "#1da1f2";
+    number.css("color","#1da1f2");
   }
 }
 
 // FUNÇÃO DESATIVA/ATIVA BOTÃO TWEET
-function disable() {
-  let valueLength = document.querySelector(".tweet-input").value.length;
-  if (tweetInput.value !== "" && valueLength <= 140) {
-    button.removeAttribute("disabled", "false");
-  } else if (tweetInput.value === "" || valueLength > 140) {
-    button.setAttribute("disabled", "true");
+let disable = () => {
+  const value = $(".tweet-input").val();
+  if (value !== "" && value.length <= 140) {
+    $(".btn").removeAttr("disabled");
+  } else if (value === "" || value.length > 140) {
+    $(".btn").attr("disabled", "true");
   }
 }
 
@@ -115,28 +112,21 @@ window.onload = function () {
   }
 };
 
-function edit() {
-  let inputName = document.createElement("input");
-  let inputUser = document.createElement("input");
-  inputName.setAttribute("id", "newName");
-  inputUser.setAttribute("id", "newUser");
-  name.appendChild(inputName);
-  user.appendChild(inputUser);
-  inputName.setAttribute("placeholder", "Novo nome");
-  inputUser.setAttribute("placeholder", "Novo usuário");
-  btnSave.style.display = "inline";
-  btnEdit.style.display = "none";
+let edit = (name, user, save, edit) => {
+  name.append(`<input id="newName" placeholder="Novo nome"></input>`);
+  user.append(`<input id="newUser" placeholder="Novo usuário"></input>`);
+  save.fadeIn();
+  edit.fadeOut(1);
 }
 
 
-function save() {
-  let inputNameValue = document.getElementById("newName");
-  let inputUserValue = document.getElementById("newUser");
-  name.removeChild(inputNameValue);
-  user.removeChild(inputUserValue);
-  if (inputNameValue.value !== "") { name.textContent = inputNameValue.value; }
-  if (inputUserValue.value !== "") { user.textContent = "@" + inputUserValue.value; }
-  btnSave.style.display = "none";
-  btnEdit.style.display = "inline";
+let save = (name, user, save, edit) => {
+  const inputNameValue = $("#newName");
+  const inputUserValue = $("#newUser");
+  if (inputNameValue.val() !== "") { name.text(inputNameValue.val()); }
+  if (inputUserValue.val() !== "") { user.text(`@${inputUserValue.val()}`); }
+  inputNameValue.remove();
+  inputUserValue.remove();
+  save.fadeOut(1);
+  edit.fadeIn();
 }
-
